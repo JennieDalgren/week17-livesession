dc8cb44954baffaa3ae434c62d3cdfca8efe7a52
Jennie Dalgren <jennie.dalgren@gmail.com>
2021-12-15 13:54:01 +0100

    finished code after wednesday live session

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
-
 const Company = mongoose.model('Company', {
   index: Number,
   company: String,
@@ -40,9 +36,53 @@ if (process.env.RESET_DB) {
   seedDatabase()
 }
 
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
+
+// Our own middleware that checks if the database is connected before going forward to our endpoints
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
 // This is our first endpoint
 app.get('/', (req, res) => {
   res.send('Hello from us!')
+})
+
+// get all the companies
+app.get('/companies', async (req, res) => {
+  console.log(req.query)
+  let companies = await Company.find(req.query)
+
+  // if we want to filter the list where funding amount is greater than whatever query value we get in, we need to specify that separately
+  if (req.query.fundingAmountUSD) {
+    const comapniesByAmount = await Company.find().gt(
+      'fundingAmountUSD',
+      req.query.fundingAmountUSD
+    )
+    companies = comapniesByAmount
+  }
+
+  res.json(companies)
+})
+
+// get one company based on id
+app.get('/companies/id/:id', async (req, res) => {
+  try {
+    const companyById = await Company.findById(req.params.id)
+    if (companyById) {
+      res.json(companyById)
+    } else {
+      res.status(404).json({ error: 'Company not found' })
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Id is invalid' })
+  }
 })
 
 // Start the server
